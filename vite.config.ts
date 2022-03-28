e04f1980b576e920cf656a115b9889ba092d3e9c
Alexander Treder <alexander.treder@bertelsmann.de>
2022-03-28 11:24:31 +0200

    another try for vite and vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +10,5 @@ export default defineConfig({
   },
   plugins: [
     reactRefresh(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
   ],
 })
